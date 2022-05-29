58c4cf68b5bb950b704e1d9a1ca2311bdfd1861c
xHopelesss <66902752+xHopelesss@users.noreply.github.com>
2022-05-30 02:22:10 +1000

    Add files via upload

---
+++ first.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+echo "Automatic Ubuntu Set-up"
+
+echo "== Please input sudo password =="
+	sudo -l > /dev/null
+
+#Updates and upgrades user system
+echo "== Updating and upgrading system =="
+sudo apt-get -y update > /dev/null
+sudo apt-get -y upgrade > /dev/null
+echo "== System updated and upgraded. =="
+
+if [[ -d Tools/ ]] 
+then
+echo "Directory already exists, exiting.."
+	exit 0
+fi
+
+mkdir Tools 
+cd Tools/
+echo "== Tools directory created =="
+
+sudo apt-get install -y git > /dev/null
+sudo apt-get install -y python3 > /dev/null
+sudo apt-get install -y python3-pip > /dev/null
+pip install pip --upgrade > /dev/null
+pip install setuptools --upgrade > /dev/null
+echo "== Dependencies installed =="
+
+echo "== Installing recon tools =="
+git clone --quiet https://github.com/sherlock-project/sherlock.git > /dev/null
+cd sherlock
+python3 -m pip install -r requirements.txt > /dev/null
+cd ..
+
+echo "== Installing scanning and enum tools =="
+sudo apt-get install -y nmap > /dev/null
+sudo apt-get install -y gobuster > /dev/null
+
+git clone --quiet https://www.github.com/threat9/routersploit
+cd routersploit
+python3 -m pip install -r requirements.txt > /dev/null
+cd ..
+
+git clone --quiet https://github.com/danielmiessler/SecLists.git > /dev/null
+
+echo "== Installing Other Tools =="
+git clone --quiet https://github.com/bitbrute/evillimiter.git
+cd evillimiter
+sudo python3 setup.py install
+cd .. 
+
+sudo apt-get install -y john > /dev/null
